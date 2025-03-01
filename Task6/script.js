2196a62e70360ad2c4369f95ae173c53fbad682b
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-03-01 21:28:49 +0530

    Add files via upload

---
+++ Task6/script.js
@@ -0,0 +1,25 @@
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let valid = true;
+
+  if (name.trim() === "") {
+    document.getElementById("nameError").innerText = "Name is required.";
+    valid = false;
+  } else {
+    document.getElementById("nameError").innerText = "";
+  }
+
+  if (email.trim() === "" || !email.includes("@")) {
+    document.getElementById("emailError").innerText = "Enter a valid email.";
+    valid = false;
+  } else {
+    document.getElementById("emailError").innerText = "";
+  }
+
+  if (valid) {
+    alert("Form submitted successfully!");
+  }
+});
